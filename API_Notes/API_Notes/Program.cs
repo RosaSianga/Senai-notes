6b822cc0d28a0bc20cb5462d18fa6252324a6213
Alcides Paz <alcidesnp@hotmail.com>
2025-05-13 14:00:16 -0300

    feat(API): Estruturaçao de diretorios da API e instalacao de Dependencias

---
+++ API_Notes/API_Notes/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Antes de Iniciar o Projeto, caso queira testar na sua máquina, descomente a linha abaixo e rode a aplicacao
+//app.MapGet("/", () => "Hello World!");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
