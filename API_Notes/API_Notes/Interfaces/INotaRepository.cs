61871cd7aa3f68a27c69ea2ca1c9245ae160752a
Alcides Paz <alcidesnp@hotmail.com>
2025-05-13 21:55:20 -0300

    update: Criação da Interface e Repository da tabela Notas

---
+++ API_Notes/API_Notes/Interfaces/INotaRepository.cs
@@ -0,0 +1,15 @@
+﻿using API_Notes.Models;
+
+namespace API_Notes.Interfaces
+{
+    public interface INotaRepository
+    {
+        List<Nota> ListarTodos();
+
+        Nota BuscarPorID(int id);
+
+        void CadastrarNota(Nota nota);
+
+        void AtualizarNota(int id, Nota nota);
+    }
+}
---
+++ API_Notes/API_Notes/Repositories/NotaRepository.cs
@@ -0,0 +1,39 @@
+﻿using API_Notes.Context;
+using API_Notes.Interfaces;
+using API_Notes.Models;
+
+namespace API_Notes.Repositories
+{
+    public class NotaRepository : INotaRepository
+    {
+        private readonly SenaiNotesContext _context;
+
+        public NotaRepository(SenaiNotesContext context)
+        {
+            _context = context;
+        }
+
+        public void AtualizarNota(int id, Nota nota)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Nota BuscarPorID(int id)
+        {
+            return _context.Notas.FirstOrDefault(n => n.IdNotas == id);
+        }
+
+        public void CadastrarNota(Nota nota)
+        {
+            Nota notaCadastrada = new Nota 
+            {
+
+            };
+        }
+
+        public List<Nota> ListarTodos()
+        {
+            return _context.Notas.ToList();
+        }
+    }
+}
