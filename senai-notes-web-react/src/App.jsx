cbec5491b6f5c9ab3bd1281fdeb11288d87c5a47
Vinicius <vinicius2145.god@gmail.com>
2025-05-15 19:52:29 -0300

    Atualizacao de fonte

--- senai-notes-web-react/src/App.jsx
+++ senai-notes-web-react/src/App.jsx
@@ -1,14 +1,32 @@
-import Anotacoes from "../../senai-notes-web-react/src/pages/Anotacoes/Anotacoes"
+
 import './App.css'
+import Login from "./pages/login";
+import Forgot from "./pages/forgot";
+import Reset from "./pages/reset";
+import Usuario from "./pages/usuario";
+import Notes from "./pages/notes";
+
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
 
 
   return (
     <>
- 
-  <Anotacoes></Anotacoes>
+      <BrowserRouter>
 
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/usuario" element={<Usuario />} />
+          <Route path="/notes" element={<Notes />} />
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
---
+++ senai-notes-web-react/src/pages/reset/index.jsx
@@ -0,0 +1,55 @@
+import './reset.css'
+import logo from '../../assets/img/logo.svg'
+
+function Reset() {
+
+
+    return (
+        <>
+
+            <header></header>
+
+            <div className="borda-reset">
+
+                <main className="page-container-reset">
+
+                    <img src={logo} alt="Logo Senai Notes" />
+
+                    <h1>Reset Your Password</h1>
+                    <p>Chose a new password to secure your account.</p>
+
+                    <div className="reset-container">
+
+                        <div className="label">
+                            <p className="descricao">New Password</p>
+                            <p></p>
+                        </div>
+
+
+                        <input className="inpt" type="password" />
+                        <p className='label-p'>At least 8 characters</p>
+
+                        <div className="label">
+                             <p className="descricao">Confirm New Password</p>
+                            <p></p>
+                        </div>
+
+                        <input className="inpt" type="password" />
+
+                        <button className="btn" >Reset Password</button>
+
+                    </div>
+
+                </main>
+
+            </div>
+
+            <footer></footer>
+
+
+
+        </>
+    )
+}
+
+export default Reset
