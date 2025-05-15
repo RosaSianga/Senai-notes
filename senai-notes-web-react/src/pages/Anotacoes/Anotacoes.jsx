c8fcaff6337dc504ab7981f353eaee76dcaea60d
Vinicius <vinicius2145.god@gmail.com>
2025-05-14 21:51:54 -0300

    fazendo-header

--- senai-notes-web-react/src/pages/Anotacoes/Anotacoes.jsx
+++ senai-notes-web-react/src/pages/Anotacoes/Anotacoes.jsx
@@ -1,6 +1,8 @@
 import "./Anotacoes.css"
 import imgcabecalho from "../../assets/imgs/logo.png"
-
+import imglupa from "../../assets/imgs/Search.png"
+import imgeng from "../../assets/imgs/Setting.png"
+import imgpes from "../../assets/imgs/Vector.png"
 
 
 function Anotacoes(){
@@ -9,13 +11,29 @@ return (
 
 <>
 
+    <main>
+    <header>
+        <div className="cabecalho">
 
-    <header className="cabecalho">
-    <h1 className="txt1">All Notes</h1>
-    <input className="inptt" type="text" placeholder="Search by title, content, or tags…"/>
+     <h1 className="txt1">All Notes</h1>
+    <input className="inptt" type="text" placeholder="Search by title, content, or tags…" />
     <img className="img-cabecalho" src={imgcabecalho} alt="" />
+    <img className="img-lupa" src={imglupa} alt="" />
+    <img className="img-engrenagem" src={imgeng} alt="" />
+    <img className="img-pessoas" src={imgpes} alt="" />
 
-    </header>
+        </div>
+
+        <div className="quadrado-left">
+
+        </div>
+        
+        <footer>
+        
+        
+        </footer>
+        </header>
+    </main>
 </>
 
 )
