c31e8a5cf970f5c025645b2ac0506e7c8d7a0740
Alcides Paz <alcidesnp@hotmail.com>
2025-05-20 11:55:27 -0300

    update: Ajsutes pontuais

--- API_Notes/API_Notes/Context/SenaiNotesContext.cs
+++ API_Notes/API_Notes/Context/SenaiNotesContext.cs
@@ -11,9 +11,12 @@ public partial class SenaiNotesContext : DbContext
     {
     }
 
-    public SenaiNotesContext(DbContextOptions<SenaiNotesContext> options)
+    private IConfiguration _configuration;
+
+    public SenaiNotesContext(DbContextOptions<SenaiNotesContext> options, IConfiguration config)
         : base(options)
     {
+        _configuration = config;
     }
 
     public virtual DbSet<ConfiguracaoUsuario> ConfiguracaoUsuarios { get; set; }
@@ -27,9 +30,13 @@ public partial class SenaiNotesContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=senainotesserver.database.windows.net;Initial Catalog=SENAI_NOTES;User Id=LoginAlcidesBack;Password=BackSenai@134;TrustServerCertificate=true;");
-
+        => optionsBuilder.UseSqlServer(
+            "Data Source=localhost, 1433;Initial Catalog=SENAI_NOTES;User Id=sa;Password=Senai@134;TrustServerCertificate=true;");
+    /*{
+        var con = _configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(con);
+    } */
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<ConfiguracaoUsuario>(entity =>
