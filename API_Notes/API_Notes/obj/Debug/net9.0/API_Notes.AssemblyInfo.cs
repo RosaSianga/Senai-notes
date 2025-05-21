53ecc42c7854f4761b5241c02a68de9ba73301b4
marcobutrico <marcobutrico@gmail.com>
2025-05-20 21:55:45 -0300

    atualizado

--- API_Notes/API_Notes/obj/Debug/net9.0/API_Notes.AssemblyInfo.cs
+++ API_Notes/API_Notes/obj/Debug/net9.0/API_Notes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aebb4d5701f7e6a5b0d8f35da73f8d604e468e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8776ecba21585d0923d29f71b06e65307b87a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
